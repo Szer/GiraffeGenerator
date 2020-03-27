aad920375cdfd5a1ff63e84a89cd31da12f61cc7
Ayrat Hudaygulov <ayrat.hudaygulov@jet.com>
2020-03-27 23:01:43 +0000

    fix build scripts

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ dotnet pack src/GiraffeGenerator.Sdk -c Release -o bin/nupkg
 dotnet pack src/GiraffeGenerator -c Release -o bin/nupkg
 dotnet mergenupkg --source bin/nupkg/GiraffeGenerator.Sdk.1.0.0.nupkg --other bin/nupkg/GiraffeGenerator.1.0.0.nupkg --tools --only-files
 dotnet pack src/GiraffeGenerator.Core -c Release -o bin/nupkg
-dotet build src/Example -c Release
+dotnet build src/Example -c Release
 dotnet test
