5371812732728445b78c5adc77b089703b28ad54
Ayrat Hudaygulov <ayrat.hudaygulov@jet.com>
2020-03-27 22:30:22 +0000

    fixed build script to include tests

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ dotnet pack src/GiraffeGenerator.Sdk -c Release -o bin/nupkg
 dotnet pack src/GiraffeGenerator -c Release -o bin/nupkg
 dotnet mergenupkg --source bin/nupkg/GiraffeGenerator.Sdk.1.0.0.nupkg --other bin/nupkg/GiraffeGenerator.1.0.0.nupkg --tools --only-files
 dotnet pack src/GiraffeGenerator.Core -c Release -o bin/nupkg
+dotnet test
