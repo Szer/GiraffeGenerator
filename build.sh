b939c392fb61a9da96d9d4c323e5b4f2bb08be66
Ayrat Hudaygulov <ayrat.hudaygulov@jet.com>
2020-03-28 00:28:59 +0000

    added some nuget publish preparations

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
+export V=$(pcregrep -o1 '(\d+\.\d+\.\d+)' Directory.build.props)
 dotnet tool restore
-dotnet pack src/GiraffeGenerator.Sdk -c Release -o bin/nupkg
-dotnet pack src/GiraffeGenerator -c Release -o bin/nupkg
-dotnet mergenupkg --source bin/nupkg/GiraffeGenerator.Sdk.1.0.0.nupkg --other bin/nupkg/GiraffeGenerator.1.0.0.nupkg --tools --only-files
-dotnet pack src/GiraffeGenerator.Core -c Release -o bin/nupkg
+dotnet pack src/GiraffeGenerator.Sdk -c Release -o bin/nupkg /p:Version=$V
+dotnet pack src/GiraffeGenerator -c Release -o bin/nupkg /p:Version=$V
+dotnet mergenupkg --source bin/nupkg/GiraffeGenerator.Sdk.$V.nupkg --other bin/nupkg/GiraffeGenerator.$V.nupkg --tools --only-files
+dotnet pack src/GiraffeGenerator.Core -c Release -o bin/nupkg /p:Version=$V
 dotnet build src/Example -c Release
 dotnet test
