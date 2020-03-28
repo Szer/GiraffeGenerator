adf9969a5d0ad829374d514f21ef8cd2ce7d3a72
Ayrat Hudaygulov <ayrat.hudaygulov@jet.com>
2020-03-28 08:17:49 +0000

    fixed CI

--- build.ps1
+++ build.ps1
@@ -3,7 +3,7 @@ $version = $matches.Matches[0].Groups[1].Value
 
 dotnet pack src/GiraffeGenerator.Sdk -c Release -o bin/nupkg /p:Version=$version
 dotnet pack src/GiraffeGenerator -c Release -o bin/nupkg /p:Version=$version
-dotnet mergenupkg --source bin/nupkg/GiraffeGenerator.Sdk.$V.nupkg --other bin/nupkg/GiraffeGenerator.$V.nupkg --tools --only-files
+dotnet mergenupkg --source bin/nupkg/GiraffeGenerator.Sdk.$version.nupkg --other bin/nupkg/GiraffeGenerator.$version.nupkg --tools --only-files
 dotnet pack src/GiraffeGenerator.Core -c Release -o bin/nupkg /p:Version=$version
 dotnet build src/Example -c Release
 dotnet test
