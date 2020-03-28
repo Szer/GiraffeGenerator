cea8e83ad1098dc7c87f9d75dac1231794e1d80f
Ayrat Hudaygulov <ayrat.hudaygulov@jet.com>
2020-03-28 14:28:20 +0000

    fixed CI

--- build.ps1
+++ build.ps1
@@ -1,6 +1,8 @@
 $matches = Select-String -path Directory.build.props '<Version>(\d+\.\d+\.\d+)</Version>'
 $version = $matches.Matches[0].Groups[1].Value
 
+rm -r - f bin
+
 dotnet pack src/GiraffeGenerator.Sdk -c Release -o bin/nupkg /p:Version=$version
 dotnet pack src/GiraffeGenerator -c Release -o bin/nupkg /p:Version=$version
 dotnet mergenupkg --source bin/nupkg/GiraffeGenerator.Sdk.$version.nupkg --other bin/nupkg/GiraffeGenerator.$version.nupkg --tools --only-files
