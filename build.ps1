72597f4d0458f96a95cd2b8428f54eef7ae06dc7
Szer <szer.penguin@gmail.com>
2020-04-08 13:17:47 +0100

    Added restore with lock file (#21)
    added restore with lock file

--- build.ps1
+++ build.ps1
@@ -1,11 +1,13 @@
 $matches = Select-String -path Directory.build.props '<Version>(\d+\.\d+\.\d+)</Version>'
 $version = $matches.Matches[0].Groups[1].Value
 
-rm -r -f bin
+Get-ChildItem .\ -include bin,obj -Recurse | foreach ($_) { remove-item $_.fullname -Force -Recurse }
 
+dotnet tool restore
+dotnet restore --locked-mode -s https://api.nuget.org/v3/index.json
 dotnet pack src/GiraffeGenerator.Sdk -c Release -o bin/nupkg /p:Version=$version
-dotnet pack src/GiraffeGenerator -c Release -o bin/nupkg /p:Version=$version
+dotnet pack src/GiraffeGenerator -c Release -o bin/nupkg /p:Version=$version --no-restore
 dotnet mergenupkg --source bin/nupkg/GiraffeGenerator.Sdk.$version.nupkg --other bin/nupkg/GiraffeGenerator.$version.nupkg --tools --only-files
-dotnet pack src/GiraffeGenerator.Core -c Release -o bin/nupkg /p:Version=$version
-dotnet build src/Example -c Release
-dotnet test
+dotnet pack src/GiraffeGenerator.Core -c Release -o bin/nupkg /p:Version=$version --no-restore
+dotnet build src/Example -c Release --no-restore
+dotnet test --no-restore
