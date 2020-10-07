62902b74867bd1b36a02ec0173d2f6b89b923932
bessgeor <bessgeor@gmail.com>
2020-10-07 19:29:40 +0300

    Extract errors DU generation to a separate file

--- src/GiraffeGenerator.Core/CodeGen.fs
+++ src/GiraffeGenerator.Core/CodeGen.fs
@@ -221,172 +221,15 @@ let giraffeAst (api: Api) =
           openDecl "System.Threading.Tasks"
           openDecl "Microsoft.AspNetCore.Http"
           
-          // generate binding errors DU and stringifiers
-          let errInnerTypeName = "ArgumentError"
-          let errInnerGiraffeBinding = "GiraffeBindingError"
-          let errInnerFormatterBindingExn = "FormatterBindingException"
-          let errInnerModelBindingUnexpectedNull = "ModelBindingUnexpectedNull"
-          let errInnerCombined = "CombinedArgumentErrors"
-          let errInnerType =
-              DU {
-                  Name = errInnerTypeName
-                  Docs = Some { Summary = Some ["Represents some concrete error somewhere in arguments"]; Example = None; Remarks = None }
-                  Cases =
-                      [
-                          {
-                              CaseName = Some errInnerGiraffeBinding
-                              Docs = Some { Summary = Some ["Giraffe error returned in Result.Error of tryBindXXX method"]; Example = None; Remarks = None }
-                              Kind = Prim <| PrimTypeKind.String StringFormat.String
-                          }
-                          {
-                              CaseName = Some errInnerFormatterBindingExn
-                              Docs = Some { Summary = Some [ "Represents exception occured during IFormatter bind" ]; Example = None; Remarks = None }
-                              Kind = BuiltIn "exn"
-                          }
-                          {
-                              CaseName = Some errInnerModelBindingUnexpectedNull
-                              Docs = Some { Summary = Some ["For IFormatter bind (JSON, for example), represents a null that happens to exist because no value was provided for a property which is required"]; Example = None; Remarks = None }
-                              Kind = TypeKind.Object
-                                  {
-                                      Name = None
-                                      Properties =
-                                          [
-                                              "TypeName", Prim (PrimTypeKind.String StringFormat.String), None
-                                              "PropertyPath", TypeKind.Array (Prim <| PrimTypeKind.String StringFormat.String), None
-                                          ]
-                                      Docs = None
-                                  }
-                          }
-                          {
-                              CaseName = Some errInnerCombined
-                              Docs = Some { Summary = Some ["Represents multiple errors occured in one location"]; Example = None; Remarks = None }
-                              Kind = TypeKind.Array (DU { Name = errInnerTypeName; Docs = None; Cases = [] })
-                          }
-                      ]
-              }
-          let errOuterTypeName = "ArgumentLocationedError"
-          let errOuterBody = "BodyBindingError"
-          let errOuterQuery = "QueryBindingError"
-          let errOuterPath = "PathBindingError"
-          let errOuterCombined = "CombinedArgumentLocationError"
-          let errOuterType =
-              DU {
-                  Name = errOuterTypeName
-                  Docs = Some { Summary = Some ["Represents error in arguments of some location"]; Example = None; Remarks = None; }
-                  Cases =
-                      [
-                          {
-                              CaseName = Some errOuterBody
-                              Docs = Some { Summary = Some ["Represents error in body"]; Example = None; Remarks = None; }
-                              Kind = errInnerType
-                          }
-                          {
-                              CaseName = Some errOuterQuery
-                              Docs = Some { Summary = Some ["Represents error in query"]; Example = None; Remarks = None; }
-                              Kind = errInnerType
-                          }
-                          {
-                              CaseName = Some errOuterPath
-                              Docs = Some { Summary = Some ["Represents error in path"]; Example = None; Remarks = None; }
-                              Kind = errInnerType
-                          }
-                          {
-                              CaseName = Some errOuterCombined
-                              Docs = Some { Summary = Some ["Represents errors in multiple locations"]; Example = None; Remarks = None; }
-                              Kind = TypeKind.Array (DU { Name = errOuterTypeName; Docs = None; Cases = [] })
-                          }
-                      ]
-              }
-          [ errInnerTypeName, errInnerType; errOuterTypeName, errOuterType ]
-          |> List.map (fun (name, kind) -> { DefaultValue = None; Name = name; Kind = kind; Docs = None })
-          |> extractRecords
-          |> types
-          
-          let innerErrToStringName = "argErrorToString"
-          let levelParam = "level"
-          let valueParam = "value"
-          let sepVar = "sep"
-          let err = "err"
-          let nextLevel = app (appI (identExpr "op_Addition") (identExpr levelParam)) (SynConst.Int32 1 |> constExpr)
-          let letSep =
-              letOrUseDecl
-                  sepVar
-                  []
-                  (
-                      app
-                          (longIdentExpr "System.String")
-                          (
-                              tupleComplexExpr
-                                [
-                                    SynConst.Char ' ' |> constExpr
-                                    app (appI (identExpr "op_Multiply") (identExpr levelParam)) (SynConst.Int32 2 |> constExpr)
-                                ]
-                          )
-                  )
-          
-          let innerErrToStringDecl =
-              letDecl true innerErrToStringName [levelParam; valueParam] None
-              ^ letSep
-              ^ simpleValueMatching valueParam
-                [
-                    errInnerGiraffeBinding, err, sprintfExpr "%sGiraffe binding error: %s" [identExpr sepVar; identExpr err]
-                    errInnerModelBindingUnexpectedNull, err,
-                        sprintfExpr "%sUnexpected null was found at path %s.%s"
-                        ^ [identExpr sepVar; longIdentExpr (sprintf "%s.TypeName" err); paren (app (app (longIdentExpr "String.concat") (strExpr ".")) (longIdentExpr (sprintf "%s.PropertyPath" err))) ]
-                    errInnerFormatterBindingExn, err, longIdentExpr (sprintf "%s.Message" err)
-                    errInnerCombined, err,
-                        sprintfExpr "%sMultiple errors:\\n%s"
-                        ^ [identExpr sepVar
-                           app
-                            (app (longIdentExpr "String.concat") (strExpr "\\n"))
-                            (
-                                app // Seq.map (recCall (level + 1))
-                                    (app (longIdentExpr "Seq.map") (paren(app (identExpr innerErrToStringName) (paren(nextLevel)))))
-                                    (identExpr err)
-                                |> paren
-                            )
-                           |> paren
-                        ]
-                ]
-          innerErrToStringDecl
-          
-          let callInnerWithFormat format var =
-              sprintfExpr format [ identExpr sepVar; paren ( app (app (identExpr innerErrToStringName) (paren nextLevel)) (identExpr var)) ]
-          
-          let outerErrToStringName = "argLocationErrorToString"
-          let outerErrToStringDecl =
-              letDecl true outerErrToStringName [levelParam; valueParam] None
-              ^ letSep
-              ^ simpleValueMatching valueParam
-                [
-                    let common =
-                        [
-                            errOuterBody, "body"
-                            errOuterPath, "path"
-                            errOuterQuery, "query"
-                        ]
-                    for (case, var) in common do
-                        let uppercase = System.String([| System.Char.ToUpperInvariant var.[0]; yield! var |> Seq.skip 1 |])
-                        let format = sprintf "%%s%s binding error:\\n%%s" uppercase
-                        case, var, (callInnerWithFormat format var)
-                        
-                    errOuterCombined, err,
-                         sprintfExpr "%sMultiple binding errors:\\n%s"
-                         ^ [identExpr sepVar
-                            app
-                             (app (longIdentExpr "String.concat") (strExpr "\\n\\n"))
-                             (
-                                 app // Seq.map (recCall (level + 1))
-                                     (app (longIdentExpr "Seq.map") (paren(app (identExpr outerErrToStringName) (paren(nextLevel)))))
-                                     (identExpr err)
-                                 |> paren
-                             )
-                            |> paren
-                         ]
-                ]
-          outerErrToStringDecl
-          
-          // generate helper functions for binding
+          [ CodeGenErrorsDU.errInnerTypeName, CodeGenErrorsDU.errInnerType
+            CodeGenErrorsDU.errOuterTypeName, CodeGenErrorsDU.errOuterType ]
+            |> List.map (fun (name, kind) -> { DefaultValue = None; Name = name; Kind = kind; Docs = None })
+            |> extractRecords
+            |> types
+          CodeGenErrorsDU.innerErrToStringDecl
+          CodeGenErrorsDU.outerErrToStringDecl
+
+          // generate helper functions for null checking
           let checkForUnexpectedNullsName = "checkForUnexpectedNulls"
           let checkForUnexpectedNulls =
               let checkers = "checkers"
@@ -398,7 +241,7 @@ let giraffeAst (api: Api) =
                     (
                          letOrUseComplexParametersDecl "v" (Pats [SynPat.Wild(r)])
                             (
-                                app (identExpr errInnerModelBindingUnexpectedNull)
+                                app (identExpr CodeGenErrorsDU.errInnerModelBindingUnexpectedNull)
                                     (
                                         recordExpr
                                             [
@@ -736,7 +579,7 @@ let giraffeAst (api: Api) =
                                             app
                                                 (typeApp (longIdentExpr "ctx.TryBindQueryString") [extractResponseSynType bindingKind])
                                                 (longIdentExpr "System.Globalization.CultureInfo.InvariantCulture")
-                                            ^|> appResultMapError (identExpr errInnerGiraffeBinding ^>> identExpr errOuterQuery)
+                                            ^|> appResultMapError (identExpr CodeGenErrorsDU.errInnerGiraffeBinding ^>> identExpr CodeGenErrorsDU.errOuterQuery)
                                         if not bindingTypeDiffersFromQueryType then
                                             bindRaw
                                         else
@@ -751,7 +594,7 @@ let giraffeAst (api: Api) =
                                     |> Option.map
                                         (
                                             fun p ->
-                                                let res = typeApp (identExpr "Result") [extractResponseSynType p.Kind; synType errOuterTypeName]
+                                                let res = typeApp (identExpr "Result") [extractResponseSynType p.Kind; synType CodeGenErrorsDU.errOuterTypeName]
                                                 let okCall = SynExpr.DotGet(res,r,longIdentWithDots "Ok",r)
                                                 app okCall (identExpr "pathArgs")
                                         )
@@ -829,8 +672,8 @@ let giraffeAst (api: Api) =
                                                     letBangExpr bodyBinding call (returnExpr ^ app (identExpr "Ok") (identExpr bodyBinding))
                                                 let catchClause =
                                                     [
-                                                        returnExpr (app (identExpr errInnerFormatterBindingExn) (identExpr "e"))
-                                                        ^|> identExpr errOuterBody ^|> identExpr "Error" 
+                                                        returnExpr (app (identExpr CodeGenErrorsDU.errInnerFormatterBindingExn) (identExpr "e"))
+                                                        ^|> identExpr CodeGenErrorsDU.errOuterBody ^|> identExpr "Error" 
                                                         |> clause (SynPat.Named(SynPat.Wild r, ident "e", false, None, r))
                                                     ]
                                                 taskBuilder
@@ -889,7 +732,7 @@ let giraffeAst (api: Api) =
                                                                                           app (appI (identExpr "op_GreaterThan") (longIdentExpr "errs.Length")) (constExpr ^ SynConst.Int32 1)
                                                                                       )
                                                                                       (app (longIdentExpr "Array.head") (identExpr "errs"))
-                                                                                      ^ app (identExpr errOuterCombined) (identExpr "errs")
+                                                                                      ^ app (identExpr CodeGenErrorsDU.errOuterCombined) (identExpr "errs")
                                                                             )
                                                                     errExpr ^|> identExpr "Error"
                                                             ]
@@ -961,11 +804,11 @@ let giraffeAst (api: Api) =
                           if maybeParams.IsSome then
                               let errorHandlerName = method.Name + "InputError"
                               // abstract {method.Name}InputError: error * HttpContext -> Task<{returnType}>
-                              abstractMemberDfn xmlEmpty errorHandlerName (tuple [synType errOuterTypeName; synType "HttpContext"] ^-> taskOf [ returnType ])
+                              abstractMemberDfn xmlEmpty errorHandlerName (tuple [synType CodeGenErrorsDU.errOuterTypeName; synType "HttpContext"] ^-> taskOf [ returnType ])
                               methodImplDefn errorHandlerName ["t"]
                                 (
                                     letOrUseComplexDecl (tuplePat ["err"; "http"]) (identExpr "t")
-                                    ^ letOrUseDecl "err" [] (app (app (identExpr outerErrToStringName) (constExpr <| SynConst.Int32 0)) (identExpr "err"))
+                                    ^ letOrUseDecl "err" [] (app (app (identExpr CodeGenErrorsDU.outerErrToStringName) (constExpr <| SynConst.Int32 0)) (identExpr "err"))
                                     ^ app (typeApp (longIdentExpr "Task.FromException") [returnType]) (paren <| app (identExpr "exn") ("err" |> identExpr))
                                 )
                           // abstract {method.Name}Output: {returnType} -> HttpHandler
---
+++ src/GiraffeGenerator.Core/CodeGenErrorsDU.fs
@@ -0,0 +1,164 @@
+﻿module CodeGenErrorsDU
+open AST
+open FSharp.Compiler.SyntaxTree
+open OpenApi
+    
+// generate binding errors DU and stringifiers
+let errInnerTypeName = "ArgumentError"
+let errInnerGiraffeBinding = "GiraffeBindingError"
+let errInnerFormatterBindingExn = "FormatterBindingException"
+let errInnerModelBindingUnexpectedNull = "ModelBindingUnexpectedNull"
+let errInnerCombined = "CombinedArgumentErrors"
+let errInnerType =
+  DU {
+      Name = errInnerTypeName
+      Docs = Some { Summary = Some ["Represents some concrete error somewhere in arguments"]; Example = None; Remarks = None }
+      Cases =
+          [
+              {
+                  CaseName = Some errInnerGiraffeBinding
+                  Docs = Some { Summary = Some ["Giraffe error returned in Result.Error of tryBindXXX method"]; Example = None; Remarks = None }
+                  Kind = Prim <| PrimTypeKind.String StringFormat.String
+              }
+              {
+                  CaseName = Some errInnerFormatterBindingExn
+                  Docs = Some { Summary = Some [ "Represents exception occured during IFormatter bind" ]; Example = None; Remarks = None }
+                  Kind = BuiltIn "exn"
+              }
+              {
+                  CaseName = Some errInnerModelBindingUnexpectedNull
+                  Docs = Some { Summary = Some ["For IFormatter bind (JSON, for example), represents a null that happens to exist because no value was provided for a property which is required"]; Example = None; Remarks = None }
+                  Kind = TypeKind.Object
+                      {
+                          Name = None
+                          Properties =
+                              [
+                                  "TypeName", Prim (PrimTypeKind.String StringFormat.String), None
+                                  "PropertyPath", TypeKind.Array (Prim <| PrimTypeKind.String StringFormat.String), None
+                              ]
+                          Docs = None
+                      }
+              }
+              {
+                  CaseName = Some errInnerCombined
+                  Docs = Some { Summary = Some ["Represents multiple errors occured in one location"]; Example = None; Remarks = None }
+                  Kind = TypeKind.Array (DU { Name = errInnerTypeName; Docs = None; Cases = [] })
+              }
+          ]
+  }
+let errOuterTypeName = "ArgumentLocationedError"
+let errOuterBody = "BodyBindingError"
+let errOuterQuery = "QueryBindingError"
+let errOuterPath = "PathBindingError"
+let errOuterCombined = "CombinedArgumentLocationError"
+let errOuterType =
+  DU {
+      Name = errOuterTypeName
+      Docs = Some { Summary = Some ["Represents error in arguments of some location"]; Example = None; Remarks = None; }
+      Cases =
+          [
+              {
+                  CaseName = Some errOuterBody
+                  Docs = Some { Summary = Some ["Represents error in body"]; Example = None; Remarks = None; }
+                  Kind = errInnerType
+              }
+              {
+                  CaseName = Some errOuterQuery
+                  Docs = Some { Summary = Some ["Represents error in query"]; Example = None; Remarks = None; }
+                  Kind = errInnerType
+              }
+              {
+                  CaseName = Some errOuterPath
+                  Docs = Some { Summary = Some ["Represents error in path"]; Example = None; Remarks = None; }
+                  Kind = errInnerType
+              }
+              {
+                  CaseName = Some errOuterCombined
+                  Docs = Some { Summary = Some ["Represents errors in multiple locations"]; Example = None; Remarks = None; }
+                  Kind = TypeKind.Array (DU { Name = errOuterTypeName; Docs = None; Cases = [] })
+              }
+          ]
+  }
+
+let innerErrToStringName = "argErrorToString"
+let levelParam = "level"
+let valueParam = "value"
+let sepVar = "sep"
+let err = "err"
+let nextLevel = app (appI (identExpr "op_Addition") (identExpr levelParam)) (SynConst.Int32 1 |> constExpr)
+let letSep =
+  letOrUseDecl
+      sepVar
+      []
+      (
+          app
+              (longIdentExpr "System.String")
+              (
+                  tupleComplexExpr
+                    [
+                        SynConst.Char ' ' |> constExpr
+                        app (appI (identExpr "op_Multiply") (identExpr levelParam)) (SynConst.Int32 2 |> constExpr)
+                    ]
+              )
+      )
+
+let innerErrToStringDecl =
+  letDecl true innerErrToStringName [levelParam; valueParam] None
+  ^ letSep
+  ^ simpleValueMatching valueParam
+    [
+        errInnerGiraffeBinding, err, sprintfExpr "%sGiraffe binding error: %s" [identExpr sepVar; identExpr err]
+        errInnerModelBindingUnexpectedNull, err,
+            sprintfExpr "%sUnexpected null was found at path %s.%s"
+            ^ [identExpr sepVar; longIdentExpr (sprintf "%s.TypeName" err); paren (app (app (longIdentExpr "String.concat") (strExpr ".")) (longIdentExpr (sprintf "%s.PropertyPath" err))) ]
+        errInnerFormatterBindingExn, err, longIdentExpr (sprintf "%s.Message" err)
+        errInnerCombined, err,
+            sprintfExpr "%sMultiple errors:\\n%s"
+            ^ [identExpr sepVar
+               app
+                (app (longIdentExpr "String.concat") (strExpr "\\n"))
+                (
+                    app // Seq.map (recCall (level + 1))
+                        (app (longIdentExpr "Seq.map") (paren(app (identExpr innerErrToStringName) (paren(nextLevel)))))
+                        (identExpr err)
+                    |> paren
+                )
+               |> paren
+            ]
+    ]
+
+let callInnerWithFormat format var =
+  sprintfExpr format [ identExpr sepVar; paren ( app (app (identExpr innerErrToStringName) (paren nextLevel)) (identExpr var)) ]
+
+let outerErrToStringName = "argLocationErrorToString"
+let outerErrToStringDecl =
+  letDecl true outerErrToStringName [levelParam; valueParam] None
+  ^ letSep
+  ^ simpleValueMatching valueParam
+    [
+        let common =
+            [
+                errOuterBody, "body"
+                errOuterPath, "path"
+                errOuterQuery, "query"
+            ]
+        for (case, var) in common do
+            let uppercase = System.String([| System.Char.ToUpperInvariant var.[0]; yield! var |> Seq.skip 1 |])
+            let format = sprintf "%%s%s binding error:\\n%%s" uppercase
+            case, var, (callInnerWithFormat format var)
+            
+        errOuterCombined, err,
+             sprintfExpr "%sMultiple binding errors:\\n%s"
+             ^ [identExpr sepVar
+                app
+                 (app (longIdentExpr "String.concat") (strExpr "\\n\\n"))
+                 (
+                     app // Seq.map (recCall (level + 1))
+                         (app (longIdentExpr "Seq.map") (paren(app (identExpr outerErrToStringName) (paren(nextLevel)))))
+                         (identExpr err)
+                     |> paren
+                 )
+                |> paren
+             ]
+    ]
+
