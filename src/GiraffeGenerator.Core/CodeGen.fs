2582aae58551bbd4e866e4a27bc1266b558e558f
bessgeor <bessgeor@gmail.com>
2020-10-07 20:34:19 +0300

    Extract type conversion to separate file

--- src/GiraffeGenerator.Core/CodeGen.fs
+++ src/GiraffeGenerator.Core/CodeGen.fs
@@ -1,38 +1,12 @@
 module CodeGen
 
-open System.Collections.Generic
 open System.Globalization
 open AST
 open ASTExt
 open OpenApi
+open OpenApiToAstTypesMatchingAndConversion
 open Fantomas
-open FSharp.Compiler.XmlDoc
 open FSharp.Compiler.SyntaxTree
-
-module XmlDoc =
-
-    /// | <{name}>
-    /// | {lines}
-    /// | </name>
-    let tag name lines =
-        [ yield "<" + name + ">"
-          yield! lines
-          yield "</" + name + ">" ]
-
-    /// | <summary>
-    /// | {lines}
-    /// | </summary>
-    let summary lines = tag "summary" lines
-
-    /// | <remarks>
-    /// | {lines}
-    /// | </remarks>
-    let remarks lines = tag "remarks" lines
-
-    /// | <example>
-    /// | {lines}
-    /// | </example>
-    let example lines = tag "example" lines
 
 /// helper function to create partial Giraffe flow:
 /// {method.Method} >=> service.{method.Name}
@@ -74,139 +48,6 @@ let createRoute (path: ParsedPath) =
         // or at least
         // ((>=>) POST (bindRoute "route/{args}" service.Post) next ctx
         paren routeExpr
-
-/// matching OpenAPI string IR to SynType
-let strFormatMatch =
-    function
-    | StringFormat.String
-    | PasswordString -> stringType
-    | Byte -> arrayOf byteType
-    | Binary -> arrayOf byteType
-    | DateString
-    | DateTimeString -> dateType
-    | Custom "uri"
-    | Custom "uriref" -> uriType
-    | Custom "uuid"
-    | Custom "guid"
-    | Custom "uid" -> guidType
-    | Custom name -> synType name
-
-/// matching OpenAPI primitive type IR to SynType
-let primTypeMatch =
-    function
-    | Any -> objType
-    | Int -> intType
-    | PrimTypeKind.Long -> int64Type
-    | PrimTypeKind.Double -> doubleType
-    | Bool -> boolType
-    | PrimTypeKind.String strFormat -> strFormatMatch strFormat
-
-/// matching type kinds in responses to create their syntatic types
-let rec extractResponseSynType =
-    function
-    | Prim primType -> primTypeMatch primType
-    | Array innerType -> arrayOf (extractResponseSynType innerType)
-    | Option innerType -> optionOf (extractResponseSynType innerType)
-    | BuiltIn builtIn -> synType builtIn
-    | Object { Name = Some name } -> synType name
-    | Object anonObject ->
-        let fields =
-            anonObject.Properties
-            |> List.map
-            ^ fun (name, typeKind, def) -> AST.ident name, extractResponseSynType typeKind
-
-        if fields.IsEmpty then objType else anonRecord fields
-    | DU du -> synType du.Name
-    | NoType -> unitType
-
-/// Creating AST XML docs from API representation
-let xml: Docs option -> PreXmlDoc =
-    function
-    | None -> PreXmlDoc.Empty
-    | Some docs ->
-        xmlDocs [ if docs.Summary.IsSome then XmlDoc.summary docs.Summary.Value
-                  if docs.Remarks.IsSome then XmlDoc.remarks docs.Remarks.Value
-                  if docs.Example.IsSome then XmlDoc.example docs.Example.Value ]
-
-/// Gets own name of the type instead of the name set upwards
-let getOwnName kind def =
-    match kind with
-    | DU du -> Some du.Name
-    | Object o -> o.Name
-    | _ -> None
-    |> Option.defaultWith def
-
-/// extract record definitions from
-let extractRecords (schemas: TypeSchema list) =
-    // store name and fields of records here
-    let recordsDict =
-        Dictionary<string, SynField list * Docs option>()
-    // store name and cases of records here
-    let duDict =
-        Dictionary<string, (string * SynType * PreXmlDoc) list * Docs option>()
-    
-    let rec extractSynType (name: string, kind: TypeKind) =
-        match kind with
-        | Prim primType -> primTypeMatch primType
-        | BuiltIn builtIn -> synType builtIn
-        | Array innerType -> arrayOf (extractSynType (getOwnName innerType (fun () -> name), innerType))
-        | Option innerType -> optionOf (extractSynType (getOwnName innerType (fun () -> name), innerType))
-        | Object objectKind ->
-
-            // extract field types
-            let fields =
-                objectKind.Properties
-                |> List.map (fun (fieldName, fieldKind, def) ->
-                    extractSynType (fieldName, fieldKind)
-                    |> field fieldName)
-
-            // add name and fields for later
-            if not ^ recordsDict.ContainsKey name then
-                recordsDict.Add(name, (fields, objectKind.Docs))
-
-            // return SynType with record name
-            synType name
-        | DU du ->
-            let cases =
-                du.Cases
-                |> List.mapi
-                   (
-                       fun idx case ->
-                           let name = case.CaseName |> Option.defaultWith (fun _ -> sprintf "Case%d" (idx + 1))
-                           let subtypeName = getOwnName case.Kind (fun _ -> name + "CaseValue")
-                           name,
-                           extractSynType(subtypeName, case.Kind),
-                           xml case.Docs
-                   )
-            if cases.Length > 0 && not ^ duDict.ContainsKey du.Name then
-                duDict.Add(du.Name, (cases, du.Docs))
-            synType name
-        | NoType -> failwith "Field without types are not supported for record schemas"
-
-    // iterate through schemas
-    // records will be stored in dictionary as a side effect
-    for schema in schemas do
-        extractSynType (schema.Name, schema.Kind)
-        |> ignore
-    
-    // create final DU expressions
-    let dus =    
-        duDict
-        |> Seq.map
-        ^ fun (KeyValue(name, (cases, docs))) ->
-            discriminatedUnion (xml docs) name cases
-
-    // create final record expressions
-    let records =
-        recordsDict
-        |> Seq.map
-        ^ fun (KeyValue (name, (fields, docs))) ->
-            let xmlDocs = xml docs
-            record xmlDocs name fields
-    
-    dus
-    |> Seq.append records
-    |> Seq.toList
 
 let requestCommonInputTypeName (method: PathMethodCall) = method.Name + method.Method + "Input"
 let sourceSorting (source: PayloadLocation) = source = Path, source = Query, source = Body(Json), source = Body(Form)
@@ -457,35 +298,6 @@ let giraffeAst (api: Api) =
                              // <summary>{method.Docs}</summary>
                              // abstract {method.Name}: HttpHandler
                              abstractGetterDfn (xml method.Docs) method.Name (synType "HttpHandler")
-
-                      let rec defaultToExpr v =
-                          let inline cnst syn v =
-                              constExpr (syn v)
-                          match v with
-                          | DefaultableKind.Boolean b -> cnst SynConst.Bool b
-                          | DefaultableKind.Date d ->
-                              let components =
-                                  [
-                                      d.Year
-                                      d.Month
-                                      d.Day
-                                  ] |> List.map (cnst SynConst.Int32)
-                              app (longIdentExpr "System.DateTime") (tupleComplexExpr components)
-                          | DefaultableKind.Double d -> cnst SynConst.Double d
-                          | DefaultableKind.Guid g ->
-                              let gString = g.ToString("D", CultureInfo.InvariantCulture)
-                              app (longIdentExpr "System.Guid.ParseExact") (tupleComplexExpr [strExpr gString; strExpr "D"])
-                          | DefaultableKind.Integer i -> cnst SynConst.Int32 i
-                          | DefaultableKind.Long l -> cnst SynConst.Int64 l
-                          | DefaultableKind.String s -> strExpr s
-                          | DefaultableKind.Uri u ->
-                              let uString = u.ToString()
-                              app (longIdentExpr "System.Uri") (strExpr uString)
-                          | DefaultableKind.DateTime dt ->
-                              let dtTicks = dt.DateTime.Ticks |> DefaultableKind.Long |> defaultToExpr
-                              let tsTicks = dt.Offset.Ticks |> DefaultableKind.Long |> defaultToExpr
-                              let ts = app (longIdentExpr "System.TimeSpan.FromTicks") (tupleComplexExpr [ tsTicks ])
-                              app (longIdentExpr "System.DateTimeOffset") (tupleComplexExpr [ dtTicks; ts ])
                       
                       let rec generateDefaultRecordMapping instanceName source =
                           let source = match source with TypeKind.Object o -> o | _ -> failwith "source should be an object"
@@ -530,28 +342,7 @@ let giraffeAst (api: Api) =
                               |> Option.map (fun param -> tuple [ param; synType "HttpContext" ])
                               // HttpContext
                               |> Option.defaultValue (synType "HttpContext")
-                             
-                          let rec generateOptionalType kind def =
-                              match kind with
-                              | TypeKind.Object o ->
-                                  let mutable hasPropertiesWithDefault = false
-                                  let kind =
-                                      {
-                                          o with
-                                              Name = None
-                                              Properties =
-                                                  o.Properties
-                                                  |> List.map ^ fun (name, kind, def) ->
-                                                      let (hasDefault, kind) = generateOptionalType kind def
-                                                      if hasDefault then
-                                                          hasPropertiesWithDefault <- true
-                                                      name, kind, def
-                                      }
-                                      |> TypeKind.Object
-                                  hasPropertiesWithDefault, kind
-                              | v ->
-                                  let isDefaultable = Option.isSome def
-                                  isDefaultable, if isDefaultable then TypeKind.Option v else v
+                              
                           // helper for emitting:                                                                             
                           // override {implDefn} = fun next ctx ->task {                                                      
                           //     (*optional*) let query = ctx.TryBindQuery<TQuery>()
---
+++ src/GiraffeGenerator.Core/OpenApiToAstTypesMatchingAndConversion.fs
@@ -0,0 +1,219 @@
+﻿module OpenApiToAstTypesMatchingAndConversion
+
+open System.Collections.Generic
+open System.Globalization
+open AST
+open ASTExt
+open FSharp.Compiler.SyntaxTree
+open FSharp.Compiler.XmlDoc
+open OpenApi
+
+module XmlDoc =
+
+    /// | <{name}>
+    /// | {lines}
+    /// | </name>
+    let tag name lines =
+        [ yield "<" + name + ">"
+          yield! lines
+          yield "</" + name + ">" ]
+
+    /// | <summary>
+    /// | {lines}
+    /// | </summary>
+    let summary lines = tag "summary" lines
+
+    /// | <remarks>
+    /// | {lines}
+    /// | </remarks>
+    let remarks lines = tag "remarks" lines
+
+    /// | <example>
+    /// | {lines}
+    /// | </example>
+    let example lines = tag "example" lines
+
+/// matching OpenAPI string IR to SynType
+let strFormatMatch =
+    function
+    | StringFormat.String
+    | PasswordString -> stringType
+    | Byte -> arrayOf byteType
+    | Binary -> arrayOf byteType
+    | DateString
+    | DateTimeString -> dateType
+    | Custom "uri"
+    | Custom "uriref" -> uriType
+    | Custom "uuid"
+    | Custom "guid"
+    | Custom "uid" -> guidType
+    | Custom name -> synType name
+
+/// matching OpenAPI primitive type IR to SynType
+let primTypeMatch =
+    function
+    | Any -> objType
+    | Int -> intType
+    | PrimTypeKind.Long -> int64Type
+    | PrimTypeKind.Double -> doubleType
+    | Bool -> boolType
+    | PrimTypeKind.String strFormat -> strFormatMatch strFormat
+
+/// matching type kinds in responses to create their syntatic types
+let rec extractResponseSynType =
+    function
+    | Prim primType -> primTypeMatch primType
+    | Array innerType -> arrayOf (extractResponseSynType innerType)
+    | Option innerType -> optionOf (extractResponseSynType innerType)
+    | BuiltIn builtIn -> synType builtIn
+    | Object { Name = Some name } -> synType name
+    | Object anonObject ->
+        let fields =
+            anonObject.Properties
+            |> List.map
+            ^ fun (name, typeKind, def) -> AST.ident name, extractResponseSynType typeKind
+
+        if fields.IsEmpty then objType else anonRecord fields
+    | DU du -> synType du.Name
+    | NoType -> unitType
+
+/// Creating AST XML docs from API representation
+let xml: Docs option -> PreXmlDoc =
+    function
+    | None -> PreXmlDoc.Empty
+    | Some docs ->
+        xmlDocs [ if docs.Summary.IsSome then XmlDoc.summary docs.Summary.Value
+                  if docs.Remarks.IsSome then XmlDoc.remarks docs.Remarks.Value
+                  if docs.Example.IsSome then XmlDoc.example docs.Example.Value ]
+
+/// Gets own name of the type instead of the name set upwards
+let getOwnName kind def =
+    match kind with
+    | DU du -> Some du.Name
+    | Object o -> o.Name
+    | _ -> None
+    |> Option.defaultWith def
+
+/// extract record definitions from
+let extractRecords (schemas: TypeSchema list) =
+    // store name and fields of records here
+    let recordsDict =
+        Dictionary<string, SynField list * Docs option>()
+    // store name and cases of records here
+    let duDict =
+        Dictionary<string, (string * SynType * PreXmlDoc) list * Docs option>()
+    
+    let rec extractSynType (name: string, kind: TypeKind) =
+        match kind with
+        | Prim primType -> primTypeMatch primType
+        | BuiltIn builtIn -> synType builtIn
+        | Array innerType -> arrayOf (extractSynType (getOwnName innerType (fun () -> name), innerType))
+        | Option innerType -> optionOf (extractSynType (getOwnName innerType (fun () -> name), innerType))
+        | Object objectKind ->
+
+            // extract field types
+            let fields =
+                objectKind.Properties
+                |> List.map (fun (fieldName, fieldKind, def) ->
+                    extractSynType (fieldName, fieldKind)
+                    |> field fieldName)
+
+            // add name and fields for later
+            if not ^ recordsDict.ContainsKey name then
+                recordsDict.Add(name, (fields, objectKind.Docs))
+
+            // return SynType with record name
+            synType name
+        | DU du ->
+            let cases =
+                du.Cases
+                |> List.mapi
+                   (
+                       fun idx case ->
+                           let name = case.CaseName |> Option.defaultWith (fun _ -> sprintf "Case%d" (idx + 1))
+                           let subtypeName = getOwnName case.Kind (fun _ -> name + "CaseValue")
+                           name,
+                           extractSynType(subtypeName, case.Kind),
+                           xml case.Docs
+                   )
+            if cases.Length > 0 && not ^ duDict.ContainsKey du.Name then
+                duDict.Add(du.Name, (cases, du.Docs))
+            synType name
+        | NoType -> failwith "Field without types are not supported for record schemas"
+
+    // iterate through schemas
+    // records will be stored in dictionary as a side effect
+    for schema in schemas do
+        extractSynType (schema.Name, schema.Kind)
+        |> ignore
+    
+    // create final DU expressions
+    let dus =    
+        duDict
+        |> Seq.map
+        ^ fun (KeyValue(name, (cases, docs))) ->
+            discriminatedUnion (xml docs) name cases
+
+    // create final record expressions
+    let records =
+        recordsDict
+        |> Seq.map
+        ^ fun (KeyValue (name, (fields, docs))) ->
+            let xmlDocs = xml docs
+            record xmlDocs name fields
+    
+    dus
+    |> Seq.append records
+    |> Seq.toList
+
+let rec generateOptionalType kind def =
+    match kind with
+    | TypeKind.Object o ->
+      let mutable hasPropertiesWithDefault = false
+      let kind =
+          {
+              o with
+                  Name = None
+                  Properties =
+                      o.Properties
+                      |> List.map ^ fun (name, kind, def) ->
+                          let (hasDefault, kind) = generateOptionalType kind def
+                          if hasDefault then
+                              hasPropertiesWithDefault <- true
+                          name, kind, def
+          }
+          |> TypeKind.Object
+      hasPropertiesWithDefault, kind
+    | v ->
+      let isDefaultable = Option.isSome def
+      isDefaultable, if isDefaultable then TypeKind.Option v else v
+      
+
+let rec defaultToExpr v =
+    let inline cnst syn v =
+        constExpr (syn v)
+    match v with
+    | DefaultableKind.Boolean b -> cnst SynConst.Bool b
+    | DefaultableKind.Date d ->
+        let components =
+            [
+              d.Year
+              d.Month
+              d.Day
+            ] |> List.map (cnst SynConst.Int32)
+        app (longIdentExpr "System.DateTime") (tupleComplexExpr components)
+    | DefaultableKind.Double d -> cnst SynConst.Double d
+    | DefaultableKind.Guid g ->
+        let gString = g.ToString("D", CultureInfo.InvariantCulture)
+        app (longIdentExpr "System.Guid.ParseExact") (tupleComplexExpr [strExpr gString; strExpr "D"])
+    | DefaultableKind.Integer i -> cnst SynConst.Int32 i
+    | DefaultableKind.Long l -> cnst SynConst.Int64 l
+    | DefaultableKind.String s -> strExpr s
+    | DefaultableKind.Uri u ->
+        let uString = u.ToString()
+        app (longIdentExpr "System.Uri") (strExpr uString)
+    | DefaultableKind.DateTime dt ->
+        let dtTicks = dt.DateTime.Ticks |> DefaultableKind.Long |> defaultToExpr
+        let tsTicks = dt.Offset.Ticks |> DefaultableKind.Long |> defaultToExpr
+        let ts = app (longIdentExpr "System.TimeSpan.FromTicks") (tupleComplexExpr [ tsTicks ])
+        app (longIdentExpr "System.DateTimeOffset") (tupleComplexExpr [ dtTicks; ts ])
